5adeef9eef18a5cb5c8a77a827fed80b366463e6
Shadi Isshac <shadi@Shadis-MacBook-Pro.local>
2022-05-19 17:45:13 -0500

    button

--- src/App.js
+++ src/App.js
@@ -2,46 +2,45 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useEffect } from "react";
 import { Component } from "react";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import MealList from "./MealList";
 
-function App() {
 
+function App() {
   const [meal, setMeal] = useState(null);
   const [calories, setCalories] = useState("2000");
 
-  function handleChange (e) {
+  function handleChange(e) {
     setCalories(e.target.value);
-
   }
-useEffect(() => {
-  getMeal()
-},[])
-  function getMeal (){
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=d299386456af4cce8794dafca7cc4f14`)
+  
+  useEffect(() => {
+    getMeal();
+  }, []);
+  
+  function getMeal() {
+  
+
+    fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=d299386456af4cce8794dafca7cc4f14`
+    )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+          setMeal (data);
+        })
+          .catch(() => {
+            console.log("error");
+          });
   }
-
-
-  return(
-    <div>
-      <section className="userData">
-      <input
-      type= "number"
-      placeholder="Calories"
-      onChange = {handleChange} />
-
-      </section>
-
-
     
 
-   </div>
+  return (
+    <div>
+      <section className="userData">
+        <input type="number" placeholder="Calories" onChange={handleChange} />
+      </section>
+      <button onClick={getMeal}>Get Meal Plan</button>
+    </div>
   );
-  
-  }
+}
 export default App;
-   
-
-
